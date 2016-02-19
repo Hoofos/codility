dc8f4662cfa4348eb11fe4ff3384459069e10bec
Henrique Thorp Küsel <hoofos@gmail.com>
2016-02-18 22:44:28 -0200

    Added CyclicRotation.cs

---
+++ CyclicRotation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Solution
+{
+    public int[] solution(int k, int[] A)
+    {
+        int[] arrRotated = new int[A.Length]; // rotated array
+        int newPos = 0;
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            newPos = i + k;
+
+            if (newPos >= A.Length)
+            {
+                newPos = newPos % A.Length;
+            }
+            arrRotated[newPos] = A[i];
+        }
+
+        return arrRotated;
+
+    }
+}
